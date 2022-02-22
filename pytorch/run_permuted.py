e8ea4b887d1c526bfb9da769a8babd143c8e71b4
vishaal27 <vishaal16119@iiitd.ac.in>
2022-02-22 17:09:56 +0000

    update run_permuted to incorporate GAN exps

--- pytorch/run_permuted.py
+++ pytorch/run_permuted.py
@@ -14,26 +14,34 @@ num_tasks = 10
 seed_everything()
 
 #Just VCL
-coreset_size = 0
-data_gen = PermutedMnistGenerator(num_tasks)
-vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-    coreset.rand_from_batch, coreset_size, batch_size, single_head)
-np.save("./results/VCL{}".format(""), vcl_result)
-print(vcl_result)
+# coreset_size = 0
+# data_gen = PermutedMnistGenerator(num_tasks)
+# vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+#     coreset.rand_from_batch, coreset_size, batch_size, single_head)
+# np.save("./results/VCL{}".format(""), vcl_result)
+# print(vcl_result)
 
 #VCL + Random Coreset
 for coreset_size in [200,400,1000,2500,5000]:
     data_gen = PermutedMnistGenerator(num_tasks)
     rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-        coreset.rand_from_batch, coreset_size, batch_size, single_head, gan_bol=True)
-    np.save("./results/rand-VCL-{}".format(coreset_size), rand_vcl_result)
+        coreset.rand_from_batch, coreset_size, batch_size, single_head)
+    np.save("./results/rand-VCL-nonvgr{}".format(coreset_size), rand_vcl_result)
     print(rand_vcl_result)
 
+#VCL + Random Coreset + VGR
+# for coreset_size in [200,400,1000,2500,5000]:
+#     data_gen = PermutedMnistGenerator(num_tasks)
+#     rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+#         coreset.rand_from_batch, coreset_size, batch_size, single_head, gan_bol=True)
+#     np.save("./results/rand-VCL{}".format(coreset_size), rand_vcl_result)
+#     print(rand_vcl_result)
+
 #VCL + k-center coreset
-coreset_size = 200
-data_gen = PermutedMnistGenerator(num_tasks)
-kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-    coreset.k_center, coreset_size, batch_size, single_head)
-print(kcen_vcl_result)
-np.save("./results/kcen-VCL{}".format(coreset_size), kcen_vcl_result)
+# coreset_size = 200
+# data_gen = PermutedMnistGenerator(num_tasks)
+# kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+#     coreset.k_center, coreset_size, batch_size, single_head)
+# print(kcen_vcl_result)
+# np.save("./results/kcen-VCL{}".format(coreset_size), kcen_vcl_result)
 
