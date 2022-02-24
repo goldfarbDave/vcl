7f6d1bf4d2a80f9a73bddff346d88e6b541962aa
AlejandroSantorum <alejandro.santorum@gmail.com>
2022-02-24 15:54:55 +0000

    Improved scripts to run several seeds and use VGR

--- pytorch/run_permuted.py
+++ pytorch/run_permuted.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import random
 import utils.vcl as vcl
 import utils.coreset as coreset
 from utils.DataGenerator import PermutedMnistGenerator
@@ -13,35 +15,46 @@ num_tasks = 10
 
 seed_everything()
 
-#Just VCL
-# coreset_size = 0
-# data_gen = PermutedMnistGenerator(num_tasks)
-# vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-#     coreset.rand_from_batch, coreset_size, batch_size, single_head)
-# np.save("./results/VCL{}".format(""), vcl_result)
-# print(vcl_result)
 
-#VCL + Random Coreset
-for coreset_size in [200,400,1000,2500,5000]:
+N_SEEDS = 3
+
+for i in range(1, N_SEEDS+1):
+    # seed everything
+    torch.manual_seed(i)
+    torch.cuda.manual_seed(i)
+    torch.cuda.manual_seed_all(i)
+    np.random.seed(i)
+    random.seed(i)
+
+    #Just VCL
+    coreset_size = 0
     data_gen = PermutedMnistGenerator(num_tasks)
-    rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-        coreset.rand_from_batch, coreset_size, batch_size, single_head)
-    np.save("./results/rand-VCL-nonvgr{}".format(coreset_size), rand_vcl_result)
-    print(rand_vcl_result)
+    vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+                    coreset.rand_from_batch, coreset_size, batch_size, single_head)
+    print(vcl_result)
+    np.save("./results/new_exp/VCL{}-seed{}".format("", i), vcl_result)
 
-#VCL + Random Coreset + VGR
-# for coreset_size in [200,400,1000,2500,5000]:
-#     data_gen = PermutedMnistGenerator(num_tasks)
-#     rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-#         coreset.rand_from_batch, coreset_size, batch_size, single_head, gan_bol=True)
-#     np.save("./results/rand-VCL{}".format(coreset_size), rand_vcl_result)
-#     print(rand_vcl_result)
+    #VCL + Random Coreset
+    for coreset_size in [200,400,1000,2500,5000]:
+        data_gen = PermutedMnistGenerator(num_tasks)
+        rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+                            coreset.rand_from_batch, coreset_size, batch_size, single_head)
+        print(rand_vcl_result)
+        np.save("./results/new_exp/rand-VCL-nonVGR-{}-seed{}".format(coreset_size, i), rand_vcl_result)
 
-#VCL + k-center coreset
-# coreset_size = 200
-# data_gen = PermutedMnistGenerator(num_tasks)
-# kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-#     coreset.k_center, coreset_size, batch_size, single_head)
-# print(kcen_vcl_result)
-# np.save("./results/kcen-VCL{}".format(coreset_size), kcen_vcl_result)
+    #VCL + Random Coreset + VGR
+    for coreset_size in [200,400,1000,2500,5000]:
+        data_gen = PermutedMnistGenerator(num_tasks)
+        rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+                            coreset.rand_from_batch, coreset_size, batch_size, single_head, gan_bol=True)
+        print(rand_vcl_result)
+        np.save("./results/new_exp/rand-VCL-VGR-{}-seed{}".format(coreset_size, i), rand_vcl_result)
+
+    #VCL + k-center coreset
+    coreset_size = 200
+    data_gen = PermutedMnistGenerator(num_tasks)
+    kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+                        coreset.k_center, coreset_size, batch_size, single_head)
+    print(kcen_vcl_result)
+    np.save("./results/new_exp/kcen-VCL{}-seed{}".format(coreset_size, i), kcen_vcl_result)
 
--- pytorch/run_split.py
+++ pytorch/run_split.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import random
 import utils.vcl as vcl
 import utils.coreset as coreset
 from utils.DataGenerator import SplitMnistGenerator
@@ -12,48 +14,58 @@ no_epochs = 120
 single_head = False
 run_coreset_only = False
 
-#Just VCL
-coreset_size = 0
-data_gen = SplitMnistGenerator()
-vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-   coreset.rand_from_batch, coreset_size, batch_size, single_head)
-np.save("./results/VCL-split{}".format(""), vcl_result)
+N_SEEDS = 3
 
-#Just VCL + VGR
-coreset_size = 0
-data_gen = SplitMnistGenerator()
-vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-   coreset.rand_from_batch, coreset_size, batch_size, single_head, gan_bol=True)
-np.save("./results/VCL-split-with-VGR{}".format(""), vcl_result)
+for i in range(1, N_SEEDS+1):
+   # seed everything
+   torch.manual_seed(i)
+   torch.cuda.manual_seed(i)
+   torch.cuda.manual_seed_all(i)
+   np.random.seed(i)
+   random.seed(i)
 
-#VCL + Random Coreset
-coreset_size = 40
-data_gen = SplitMnistGenerator()
-rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-   coreset.rand_from_batch, coreset_size, batch_size, single_head,gan_bol= False)
-print(rand_vcl_result)
-np.save("./results/VCL-all-split-randcoreset{}".format(""), rand_vcl_result)
+   #Just VCL
+   coreset_size = 0
+   data_gen = SplitMnistGenerator()
+   vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+      coreset.rand_from_batch, coreset_size, batch_size, single_head)
+   np.save(f"./results/new_exp/VCL-split-seed{i}", vcl_result)
 
-#VCL + Random Coreset + VGR
-coreset_size = 40
-data_gen = SplitMnistGenerator()
-rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-   coreset.rand_from_batch, coreset_size, batch_size, single_head,gan_bol= True)
-print(rand_vcl_result)
-np.save("./results/VCL-all-split-randcoreset-VGR{}".format(""), rand_vcl_result)
+   #Just VCL + VGR
+   coreset_size = 0
+   data_gen = SplitMnistGenerator()
+   vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+      coreset.rand_from_batch, coreset_size, batch_size, single_head, gan_bol=True)
+   np.save(f"./results/new_exp/VCL-split-with-VGR-seed{i}", vcl_result)
 
-#VCL + k-center coreset
-coreset_size = 40
-data_gen = SplitMnistGenerator()
-kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-    coreset.k_center, coreset_size, batch_size, single_head)
-print(kcen_vcl_result)
-np.save("./results/VCL-all-split-kcentercoreset{}".format(""), kcen_vcl_result)
+   #VCL + Random Coreset
+   coreset_size = 40
+   data_gen = SplitMnistGenerator()
+   rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+      coreset.rand_from_batch, coreset_size, batch_size, single_head,gan_bol= False)
+   print(rand_vcl_result)
+   np.save(f"./results/new_exp/VCL-all-split-randcoreset-seed{i}", rand_vcl_result)
 
-#VCL + k-center coreset + VGR
-coreset_size = 40
-data_gen = SplitMnistGenerator()
-kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
-    coreset.k_center, coreset_size, batch_size, single_head, gan_bol=True)
-print(kcen_vcl_result)
-np.save("./results/VCL-all-split-kcentercoreset-VGR{}".format(""), kcen_vcl_result)
+   #VCL + Random Coreset with VGR
+   coreset_size = 40
+   data_gen = SplitMnistGenerator()
+   rand_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+      coreset.rand_from_batch, coreset_size, batch_size, single_head,gan_bol= True)
+   print(rand_vcl_result)
+   np.save(f"./results/new_exp/VCL-all-split-randCoreset-VGR-seed{i}", rand_vcl_result)
+
+   #VCL + k-center coreset
+   coreset_size = 40
+   data_gen = SplitMnistGenerator()
+   kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+      coreset.k_center, coreset_size, batch_size, single_head)
+   print(kcen_vcl_result)
+   np.save(f"./results/new_exp/VCL-all-split-kCenterCoreset-seed{i}", kcen_vcl_result)
+
+   #VCL + k-center coreset with VGR
+   coreset_size = 40
+   data_gen = SplitMnistGenerator()
+   kcen_vcl_result = vcl.run_vcl(hidden_size, no_epochs, data_gen,
+      coreset.k_center, coreset_size, batch_size, single_head, gan_bol=True)
+   print(kcen_vcl_result)
+   np.save(f"./results/new_exp/VCL-all-split-kCenterCoreset-VGR-seed{i}", kcen_vcl_result)
